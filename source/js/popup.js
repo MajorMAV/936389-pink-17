c6ed5cdcfcf411c4c88430de5b5b9581e35b2e64
Artem Mochalov <major-173@yandex.ru>
2019-08-13 10:54:49 +0300

    Асинхронный submit.

--- source/js/popup.js
+++ source/js/popup.js
@@ -3,9 +3,37 @@ var errorWindow = document.querySelector(".window--error");
 var succesWindow = document.querySelector(".window--succes");
 var closeButtons = document.querySelectorAll(".window__button");
 
-document.querySelector(".app-form__button").addEventListener("click", function(evt) {
+document.querySelector(".app-form__button").addEventListener("click", function (evt) {
+  // Проверяем валидна ли форма
   if (form.checkValidity()) {
-    succesWindow.classList.add("window--is-showed");
+
+    var url = form.getAttribute("action");
+    var method = form.getAttribute("method");
+
+    //Прерывем дальнешее распространение события
+    evt.preventDefault();
+
+    try {
+      // Создаем запрос
+      var requst = new XMLHttpRequest();
+      // Инициализируем запрос
+      requst.open(method, url, true);
+      // Объявляем обработчик изменения состояния запроса
+      requst.onreadystatechange = function () {
+
+        if (requst.readyState != 4) return; // (4) Овтет получен
+        // Проверяем статус ответа
+        if (requst.status != 200) {
+          errorWindow.classList.add("window--is-showed");
+        } else {
+          succesWindow.classList.add("window--is-showed");
+        }
+      };
+      // Отправляем запрос
+      requst.send(new FormData(form));
+    } catch (e) {
+      errorWindow.classList.add("window--is-showed");
+    }
   } else {
     errorWindow.classList.add("window--is-showed");
   }
