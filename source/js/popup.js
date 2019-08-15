1fd744a3bb0e2a3d2fdba74c84e27e7567d564e8
Artem Mochalov <major-173@yandex.ru>
2019-08-15 21:46:57 +0300

    Подсветка невалидных полей формы красной границей.

--- source/js/popup.js
+++ source/js/popup.js
@@ -3,8 +3,20 @@ var errorWindow = document.querySelector(".window--error");
 var succesWindow = document.querySelector(".window--succes");
 var closeButtons = document.querySelectorAll(".window__button");
 
+var formValidator = function (form) {
+  var result = true;
+  for (var i = 0; i < form.length; i++) {
+    form[i].classList.remove("invalid");
+    if (!form[i].validity.valid) {
+      result = false;
+      form[i].classList.add("invalid");
+    }
+  }
+  return result;
+}
+
 document.querySelector(".app-form__button").addEventListener("click", function(evt) {
-  if (form.checkValidity()) {
+  if (formValidator(form)) {
     succesWindow.classList.add("window--is-showed");
   } else {
     errorWindow.classList.add("window--is-showed");
