5c12b09718e15c0a36e3faec0528c6af2e245f42
Artem Mochalov <major-173@yandex.ru>
2019-08-16 17:01:45 +0300

    Исправления Б8 - цвет placeholder, Б25 - форммирование style.css

--- gulpfile.js
+++ gulpfile.js
@@ -40,6 +40,7 @@ gulp.task("css", function () {
     .pipe(postcss([
       autoprefixer()
     ]))
+    .pipe(gulp.dest("build/css"))
     .pipe(cssmin())
     .pipe(rename("style.min.css"))
     .pipe(sourcemap.write("."))
